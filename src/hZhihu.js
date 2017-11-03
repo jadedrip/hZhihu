8fc40d8693e91433d3fadbc1a89934a5f2b3a6df
jadedrip <jadedrip@gmail.com>
2017-11-03 14:57:45 +0800

    改内容、删除都有莫名的性能问题，还是隐藏靠谱

--- src/hZhihu.js
+++ src/hZhihu.js
@@ -1,43 +1,26 @@
+var h=0;
 function onAnswerItem(_, item) {
 	var v = $(item);
-	var hide = function (_, x) {
-		var zop = v.attr("data-zop");
-		var parsed = JSON.parse(zop);
-		//var inner = v.html();
-		//v.dblclick(function () {
-		//	v.html(inner);
-		//});
-		var text;
-		var aria = $(x).attr("aria-label");
-		if (aria) {
-			text = aria + $(x).text();
-		} else {
-			text = "已感谢";
-		}
-		v.html(parsed.authorName + " - " + text).attr("style", "color: #E0E0E0");
-	}
 
+var it = true;
 	v.find("button").each(function (_, x) {
 		var m = $(x);
 		if (m.hasClass("VoteButton")) {
 			if ($(x).hasClass("is-active")) {
-				hide(_, x);
-			} else {
-				$(x).click(hide);
-			}
+			  it = false;
+			} 
 		} else {
 			var svg = m.children("svg");
 			if (svg.length > 0) {
 				var text = m.text();
-				console.log(text);
+				// console.log(text);
 				if (m.text() == '取消感谢') {
-					hide(_, x);
-				} else if (text == '感谢') {
-					m.click(hide);
-				}
+				   it = false;
+				} 
 			}
 		}
 	});
+	return it;
 }
 
 $(document).ready(function () {
@@ -47,6 +30,9 @@ $(document).ready(function () {
 	}
 });
 
-$('body').on('DOMNodeInserted', 'div.ContentItem.AnswerItem', function (e) {
-	onAnswerItem( null, e.currentTarget);
+$('body').on('DOMNodeInserted', 'div.List-item', function (e) {
+	var b= onAnswerItem( null, e.currentTarget);
+	if(!b){
+      $(e.currentTarget).hide();
+	}
 });
